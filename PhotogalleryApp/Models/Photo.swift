851aa9bd9f519cd16b2572331ee243b55e06ad6e
Kusakin_Sl <kusakin.slava@inbox.ru>
2022-06-17 19:16:48 +0300

    Build 1.0.0

--- PhotogalleryApp/Models/Photo.swift
+++ PhotogalleryApp/Models/Photo.swift
@@ -9,19 +9,11 @@ import Foundation
 
 struct Photo: Decodable {
 
-    let id: String?
-    let author: String?
-    let width: Int?
-    let height: Int?
     let url: String?
     let downloadUrl: String?
 
     init(downloadUrl: String) {
         self.downloadUrl = downloadUrl
-        self.id = nil
-        self.author = nil
-        self.width = nil
-        self.height = nil
         self.url = nil
     }
 }
